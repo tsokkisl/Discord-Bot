0b540a578ff325a410a1a4e466391f4a24533d6e
tsokkisl <l3n05.ts@gmail.com>
2019-07-18 10:28:23 +0300

    Add user warning-kick after spamming

--- bot.js
+++ bot.js
@@ -1,16 +1,27 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+var users = [];
+var kickedUsers = [];
+var warnedUsers = [];
+const warnBuff = 3;
+const kickBuff = 5;
+const interval = 2000;
+const message = {
+  warningMessage: "You are sending messages too fast! If you continue like this you are going to be kicked from the server.",
+  kickMessage: "has been kicked from server. Reason: [spamming]"
+}
+
 client.on('ready', () => {
   client.user.setActivity("the channel", {type: "WATCHING"});
   let generalChannel = client.channels.get("ENTER YOUR CHANNEL ID");
   generalChannel.send("Hello, i'm the support system of this channel. If you need anything ask me!");
-});
+})
 
 client.on('message', (msg) => {
   if (msg.author == client.user) return;
 
-  msg.channel.send("Message received from user : " + msg.author.toString() + " : " + msg.content);
+  // msg.channel.send("Message received from user : " + msg.author.toString() + " : " + msg.content);
 
   if (msg.content.startsWith("!")) {
     executeCommand(msg);
@@ -45,12 +56,62 @@ client.on('message', (msg) => {
   function execClear(msg) {
     async function clear() {
       msg.delete();
-      const fetchedMessages = await msg.channel.fetchMessages({limit: 99});
-      msg.channel.bulkDelete(fetchedMessages);
+      const fetched = await msg.channel.fetchMessages({limit: 99});
+      msg.channel.bulkDelete(fetched);
     }
     clear();
   }
+
+  //user kick
+  const kickUser = async (ms, kickMsg) => {
+      kickedUsers.push(ms.author.id);
+      let usr = ms.guild.members.get(ms.author.id);
+      if (usr) {
+        usr.kick("spamming").then((member) => {
+          ms.channel.send(`<@!${ms.author.id}>, ${kickMsg}`);
+          return true;
+       }).catch(() => {
+          msg.channel.send(`Insufficient permissions to kick user <@!${msg.author.id}>!`);
+          return false;
+      });
+    }
+  }
+
+  //user warning
+  const warnUser = async (ms, warnMsg) => {
+    warnedUsers.push(ms.author.id);
+    ms.channel.send(`<@${ms.author.id}>, ${warnMsg}`);
+  }
+
+  if (msg.author.bot) return;
+  if (!msg.member || msg.channel.type !== "text" || !msg.channel.guild || !msg.guild) return;
+
+  let currentTime = Math.floor(Date.now());
+
+  users.push({
+    "user": msg.author.id,
+    "time": currentTime
+  });
+
+  var matchedUsers = 0;
+  for (var i = 0; i < users.length; i++) {
+    if (users[i].time > currentTime - interval) {
+      matchedUsers++;
+      if (matchedUsers >= warnBuff && matchedUsers < kickBuff && !warnedUsers.includes(msg.author.id)) {
+        warnUser(msg, message.warningMessage);
+      }
+      else if (matchedUsers >= kickBuff) {
+        if (!kickedUsers.includes(msg.author.id)) {
+          kickUser(msg, message.kickMessage);
+        }
+      }
+    }
+    else if (users[i].time < currentTime - interval) {
+      users.splice(i);
+      warnedUsers.splice(warnedUsers.indexOf(users[i]));
+      kickedUsers.splice(kickedUsers.indexOf(users[i]));
+    }
+  }
 });
 
-//enter token
 client.login("ENTER YOUR TOKEN");
